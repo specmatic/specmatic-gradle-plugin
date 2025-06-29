c897dd7923a6ce5870cf0273e33b5e2a7e13d4c5
Specmatic Renovate <github-service-account@specmatic.io>
2025-06-29 02:25:34 +0000

    chore(deps): update dependency io.mockk:mockk to v1.14.4

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.assertj:assertj-core:3.27.3")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.13.1")
-    testImplementation("io.mockk:mockk:1.14.2")
+    testImplementation("io.mockk:mockk:1.14.4")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 

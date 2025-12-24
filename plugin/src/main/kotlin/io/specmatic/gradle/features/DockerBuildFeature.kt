1af4afb33d6ee05b21085052b76e7de6d6a549da
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2025-12-24 22:59:53 +0530

    chore: don't publish to znsio

--- plugin/src/main/kotlin/io/specmatic/gradle/features/DockerBuildFeature.kt
+++ plugin/src/main/kotlin/io/specmatic/gradle/features/DockerBuildFeature.kt
@@ -14,7 +14,7 @@ data class DockerBuildConfig(
     internal var mainJarTaskName: String? = null,
     var imageName: String? = null,
     var extraDockerArgs: MutableList<String> = mutableListOf(),
-    var dockerOrgNames: List<String> = mutableListOf("specmatic", "znsio"),
+    var dockerOrgNames: List<String> = mutableListOf("specmatic"),
     var extraApkDependencies: List<String> = listOf(),
 )
 

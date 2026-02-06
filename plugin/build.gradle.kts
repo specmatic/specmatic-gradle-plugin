489e158e92f08672ef4c2c69c2c0fb7691bf7cfb
Specmatic Renovate <github-service-account@specmatic.io>
2026-02-06 06:20:00 +0000

    chore(deps): update dependency com.gradleup.shadow:com.gradleup.shadow.gradle.plugin to v9.3.1

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
     implementation("org.barfuin.gradle.taskinfo:org.barfuin.gradle.taskinfo.gradle.plugin:2.2.1")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.10")
     implementation("commons-codec:commons-codec:1.21.0")
-    implementation("com.gradleup.shadow:com.gradleup.shadow.gradle.plugin:9.3.0")
+    implementation("com.gradleup.shadow:com.gradleup.shadow.gradle.plugin:9.3.1")
     implementation("com.vanniktech.maven.publish:com.vanniktech.maven.publish.gradle.plugin:0.36.0")
     implementation("org.kohsuke:github-api:1.330")
     implementation("org.cyclonedx.bom:org.cyclonedx.bom.gradle.plugin:3.1.0")
